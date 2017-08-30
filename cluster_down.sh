b3c56fee5421b6c57711e494215a376327cd3a7d
aleccunningham <aleccunningham96@gmail.com>
2017-08-30 18:44:34 -0500

    fix cluster down to take the cluster down

--- cluster_down.sh
+++ cluster_down.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-DEPLOYMENT=$1
-
-if [ "DEPLOYMENT" == "" ]; then
-  echo "Needs deployment name"
-  exit 2
-fi
-
 kubectl delete -f isba/isba-service.yml
+kubectl delete -f isba/isba.yml
 
-gcloud sql instances patch ${DEPLOYMENT} --activation-policy NEVER
-gcloud container clusters delete ${DEPLOYMENT} --quiet
+gcloud sql instances patch isbadbase --activation-policy NEVER
+gcloud container clusters delete ispa --quiet
